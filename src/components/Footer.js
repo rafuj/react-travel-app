ada82a69e3abe867e01ba412a02f6ad7c7d72a49
rafuj <raihanrafuj0078@gmail.com>
2021-12-20 12:46:10 +0600

    Footer Top Links Part Complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import image from "../assets/images/footer-bg.png";
 import FooterContact from "./FooterContact";
+import FooterTop from "./FooterTop";
 export default function Footer() {
   return (
     <>
       <FooterContact />
+      <footer
+        style={{ background: `url(${image}) #031e44 repeat center center` }}
+      >
+        <FooterTop />
+      </footer>
     </>
   );
 }
