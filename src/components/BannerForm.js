bc44a2a12e712bc22d01a92a06fe31cab800504e
rafuj <raihanrafuj0078@gmail.com>
2021-12-21 21:08:06 +0600

    Banner Form Firebase Connected and Set Requested Tourist Data in Database

--- src/components/BannerForm.js
+++ src/components/BannerForm.js
@@ -1,20 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import useDestination from "../hooks/useDestination";
 import classes from "../styles/Banner.module.css";
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 export default function BannerForm() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [guest, setGuest] = useState("");
+  const [selectDest, setSelectDest] = useState("default");
+  const [success, setSuccess] = useState(false);
+  const [selectState, setSelectState] = useState(false);
+
+  const { loading, error, dest } = useDestination();
+
+  const handleClick = () => {
+    selectDest === "default" ? setSelectState(true) : setSelectState(false);
+    setInterval(() => {
+      setSelectState(false);
+    }, 5000);
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (selectDest !== "default" && startDate && endDate && guest) {
+      const res = fetch(
+        "https://travel-app-98c90-default-rtdb.asia-southeast1.firebasedatabase.app/touristRequest.json",
+        {
+          method: "POST",
+          Headers: { "content-Type": "application/json" },
+          body: JSON.stringify({
+            destination: selectDest,
+            startDate: startDate,
+            endDate: endDate,
+            guest: guest,
+          }),
+        }
+      );
+      if (res) {
+        setSuccess(true);
+        setSelectState(false);
+        setInterval(() => {
+          setSuccess(false);
+        }, 5000);
+      } else {
+        alert("Please Fill the Data");
+      }
+    }
+  };
+
   return (
-    <form className="banner__wrapper-form row g-3">
+    <form className="banner__wrapper-form row g-3" onSubmit={handleChange}>
+      {success && (
+        <div className="col-12">
+          <div class="alert alert-success">
+            Your Request is Successfully Accepted
+          </div>
+        </div>
+      )}
+      {selectState && (
+        <div className="col-12">
+          <div class="alert alert-danger">Please Select The Destination</div>
+        </div>
+      )}
       <div className="col-6">
         <div className={classes.inputContainer}>
           <label className={classes.formLabel}>
             <span>Check In</span>
-            <Input
+            <DatePicker
               className={`${classes.formControl} form-control`}
-              type="date"
-              value="Arrival Date"
-              readOnly
+              selected={startDate}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              onChange={(e) => setStartDate(e)}
+              required
             />
           </label>
         </div>
@@ -23,11 +85,15 @@ export default function BannerForm() {
         <div className={classes.inputContainer}>
           <label className={classes.formLabel}>
             <span>Check Out</span>
-            <Input
+            <DatePicker
               className={`${classes.formControl} form-control`}
-              type="date"
-              value="Departure Date"
-              readOnly
+              selected={endDate}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              onChange={(e) => setEndDate(e)}
+              required
             />
           </label>
         </div>
@@ -37,22 +103,22 @@ export default function BannerForm() {
           <label className={classes.formLabel}>
             <span>Destination</span>
             <Select
-              name="destination"
-              id="destination"
+              value={selectDest}
               className={`${classes.formControl} form-control`}
+              onChange={(e) => setSelectDest(e.target.value)}
+              required
             >
-              <option value="d1">New Jersey</option>
-              <option value="d1">Delaware</option>
-              <option value="d1">Philadelphia</option>
-              <option value="d1">New York City</option>
-              <option value="d1">San Francisco</option>
-              <option value="d1">Wilmington</option>
-              <option value="d1">Virginia</option>
-              <option value="d1">Virginia Beach</option>
-              <option value="d1">United States</option>
-              <option value="d1">California</option>
-              <option value="d1">Los Angeles</option>
-              <option value="d1">Nevada</option>
+              <option value="default">Select Destination</option>
+              {error && <option value="error">Something Went Wrong</option>}
+              {!loading && dest.length === 0 && (
+                <option value="noFound">No Destination Available</option>
+              )}
+              {!loading &&
+                dest.map((option) => (
+                  <option value={option.city} key={option.city}>
+                    {option.city}
+                  </option>
+                ))}
             </Select>
           </label>
         </div>
@@ -65,13 +131,22 @@ export default function BannerForm() {
               className={`${classes.formControl} form-control`}
               type="number"
               placeholder="Guests"
-              readOnly
+              value={guest}
+              min="0"
+              onChange={(e) => setGuest(e.target.value)}
+              required
             />
           </label>
         </div>
       </div>
       <div className="col-sm-12">
-        <Button type="submit" button text="Submit Now" fullWidth />
+        <Button
+          type="submit"
+          button
+          text="Submit Now"
+          onClick={handleClick}
+          fullWidth
+        />
       </div>
     </form>
   );
