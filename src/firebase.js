a27757edbe736f1dd04dc4bf2057bfcd163d4996
rafuj <raihanrafuj0078@gmail.com>
2021-12-22 00:35:24 +0600

    firebase.js upgraded

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey: "AIzaSyBveLvNMoWs4HbF90ZGzgPAIkmckht3-QY",
+  authDomain: "travel-app-98c90.firebaseapp.com",
+  projectId: "travel-app-98c90",
+  storageBucket: "travel-app-98c90.appspot.com",
+  messagingSenderId: "517167260604",
+  appId: "1:517167260604:web:f45d981eec357a4c6cc1b9",
+  measurementId: "G-750TWW5SLN",
+  databaseURL:
+    "https://travel-app-98c90-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
 export default app;
